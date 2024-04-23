4b5d5813a32724a9b539738f44d1fea95bdb7da1
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2024-04-23 13:12:04 +0100

    typo: removed unused code

--- src/app.tsx
+++ src/app.tsx
@@ -5,7 +5,5 @@ import './app.scss';
  * The main application component.
  */
 export function App(): JSX.Element {
-  return <>
-    Router should go here
-  </>;
+  return <>Router should go here</>;
 }
