10b7504f78bec1fd7564dc5ae2548e91c0c02370
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2024-04-23 11:07:10 +0100

    fix: husky

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App2 } from './App.tsx';
+
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
