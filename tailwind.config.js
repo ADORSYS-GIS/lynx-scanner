a1b2c0685926828b08def668e1aa38b782659210
ValantineSuh <fuhvalentinesuh@gmail.com>
2024-04-29 13:15:14 +0100

    Installed and Configured DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./src/**/*.{tsx,scss}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require('daisyui')],
+};
 
