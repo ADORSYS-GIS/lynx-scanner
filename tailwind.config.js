5aa2233e1894586f3d80ccdcdc378e132f365cf9
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2024-04-29 11:22:16 +0100

    style(code): setup commit convention

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{tsx,scss}"],
+  content: ['./src/**/*.{tsx,scss}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
