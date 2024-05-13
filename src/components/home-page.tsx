51b46b95e8bbeb70b490223d1c6caace40d6fc26
Tchikaya Ariel <tchikayaline@gmail.com>
2024-05-13 09:18:12 +0100

    fix(frontend) : updated changes

--- src/components/home-page.tsx
+++ src/components/home-page.tsx
@@ -1,24 +1,7 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from '../reducers/rootReducer'; // Import RootState
 
-import { increment } from '../reducers/someReducers'; // Import your action creator
-
-const HomePage: React.FC = () => {
-  const count = useSelector((state: RootState) => state.someReducer.count); // Use RootState to annotate state
-
-  const dispatch = useDispatch();
-
-  const handleIncrement = () => {
-    dispatch(increment());
-  };
-
-  return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={handleIncrement}>Increment</button>
-    </div>
-  );
+const Homepage: React.FC = () => {
+  return <h2>Home</h2>;
 };
 
-export default HomePage;
+export default Homepage;
