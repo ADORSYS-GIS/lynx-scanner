ea5e0cfe8aeacdf0a07403b56082082fd80ce64e
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2024-07-26 10:04:47 +0100

    fix: selector

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -3,5 +3,5 @@ import { RootState } from './types.ts';
 
 export const selectNotification = createSelector(
   (ro: RootState) => ro.notification.messages,
-  (p) => p.filter((m) => m !== 'test')
+  (p) => p.filter(({ message }) => message.length > 0)
 );
